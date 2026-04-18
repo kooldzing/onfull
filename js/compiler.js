2597c1218985facf7a57ece874f2bedd0f39804e
kooldzing <kooldezingebay@gmail.com>
2026-04-18 20:08:43 -0300

    Update compiler.js

--- js/compiler.js
+++ js/compiler.js
@@ -259,8 +259,8 @@ function clearContractTemplateCache() {
     logToTerminal(`🗑️ Cache cleared`, 'info');
 }
 
-window.deployContract = deployContract;
 window.compileContract = compileContract;
 window.clearContractTemplateCache = clearContractTemplateCache;
+
 
 
