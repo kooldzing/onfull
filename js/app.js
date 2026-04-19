7126158e0b038cd7eabccbe63a0275b73e453194
kooldzing <kooldezingebay@gmail.com>
2026-04-18 21:36:58 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -26,9 +26,9 @@ window.addEventListener('load', async () => {
     
     await new Promise(resolve => setTimeout(resolve, 2000));
     
+    await setupCodeEditor();
     setupEventListeners();
     setupPluginSwitching();
-    await setupCodeEditor();
     setupFileSystem();
     setupResizers();
     
